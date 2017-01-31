a606239bafef1da1324d5bcf14892c890cced16c
elzup <guild0105@gmail.com>
2017-01-31 14:30:06 +0900

    Print ip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 from urllib import request
 from datetime import datetime
+import socket
 
 # out netwrok host
 URL = 'https://google.com'
@@ -8,18 +9,20 @@ URL = 'https://google.com'
 # sec
 LOG_INTERVAL = 60
 
+
 def main():
     while True:
         request_log()
         time.sleep(LOG_INTERVAL)
 
 def request_log():
+    hostnames = socket.gethostbyname(socket.gethostname())
     timestamp_str = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
     try:
         with request.urlopen(URL):
-            print(timestamp_str + ':OK')
+            print('{}: OK <{}>'.format(timestamp_str, hostnames))
     except:
-        print('\033[31m{}:Error\033[0m'.format(timestamp_str))
+        print('\033[31m{}: NG <{}>\033[0m'.format(timestamp_str, hostnames))
 
 if __name__ == '__main__':
     main()
